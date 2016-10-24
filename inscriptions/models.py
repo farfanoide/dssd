f4a137d3d78c1fa589ad5fb3b0fd55dcd7ec189c
Ivan Karl <ivan6258@gmail.com>
2016-10-23 22:19:05 -0300

    adds proc files

--- inscriptions/models.py
+++ inscriptions/models.py
@@ -2,4 +2,41 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Paper(models.Model):
+    title = models.TextField(max_length=255)
+    summary = models.TextField(max_length=255)
+    topic = models.TextField(
+        max_length=255,
+        choices=[
+            ('cloud', 'cloud'),
+            ('soa', 'soa'),
+            ('ws', 'ws'),
+            ('bpm', 'bpm'),
+        ]
+    )
+    presentation_type = models.TextField(
+        max_length=255,
+        choices=(
+            ('poster', 'poster'),
+            ('conferencia', 'conferencia'),
+        )
+    )
+    state = models.TextField(
+        max_length=255,
+        choices=(
+            ('assigned', 'assigned'),
+            ('accepted', 'accepted'),
+            ('closed', 'closed'),
+            ('finalized', 'finalized'),
+        )
+    )
+    presentation_date = models.DateTimeField()
+
+class Author(models.Model):
+    name = models.TextField(max_length=255)
+    personal_email = models.EmailField()
+    google_email = models.EmailField()
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    is_collaborator = models.BooleanField(default=False)
+
+
