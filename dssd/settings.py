26adb0b3f31f3c55b60a9be27c8e5f6f739f8ed9
Ivan Karl <ivan6258@gmail.com>
2016-10-26 20:04:59 -0300

    adds crispy forms and layout

--- dssd/settings.py
+++ dssd/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'inscriptions',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
